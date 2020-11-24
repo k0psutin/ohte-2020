1593e97071cafd49926a5f82538ae6e919a22adf
k0psutin <mr.gravez@gmail.com>
2020-11-24 22:52:09 +0200

    Init player_repository.py

--- src/repositories/player_repository.py
+++ src/repositories/player_repository.py
@@ -1,5 +1,34 @@
+"""Module player_repository.py
+
+       Handles saving and loading player data.
+"""
+
+import pickle
+import os
+
+
 class PlayerRepository():
     def __init__(self):
-        # TODO check if file highscores.dat exists, if not create empty file
-        # TODO check if file save.dat exists, if not create empty file
         pass
+
+    def save(self, save_data):
+        with open('save.dat', 'wb') as file:
+            pickle.dump(save_data, file, pickle.HIGHEST_PROTOCOL)
+
+    def load(self):
+        if os.path.isfile('save.dat') is not True:
+            return None
+
+        with open('save.dat', 'rb') as file:
+            save_data = pickle.load(file)
+
+        return save_data
+
+    def is_save_empty(self):
+        if os.path.isfile('save.dat') is not True:
+            return None
+
+        save_data = None
+        with open('save.dat', 'rb') as file:
+            save_data = pickle.load(file)
+        return save_data is not None
