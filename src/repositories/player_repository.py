13f7d06eb2af8c6c90418d6b7e5d04988b604de0
k0psutin <mr.gravez@gmail.com>
2020-12-01 21:01:26 +0200

    Add player_repository.py testing

--- src/repositories/player_repository.py
+++ src/repositories/player_repository.py
@@ -8,27 +8,48 @@ import os
 
 
 class PlayerRepository():
-    def __init__(self):
-        pass
+    """Creates an instance of Player Repository.
+
+        Typical usage example:
+        player_repository = PlayerRepository()
+    """
+
+    def __init__(self, save_data_name='save.dat'):
+        """Constructor of the class
+
+        Args:
+
+            save-data-name (str, optional): Defaults to 'save.dat'.
+        """
+        self.save_data_name = save_data_name
 
     def save(self, save_data):
-        with open('save.dat', 'wb') as file:
+        with open(self.save_data_name, 'wb') as file:
             pickle.dump(save_data, file, pickle.HIGHEST_PROTOCOL)
 
     def load(self):
-        if os.path.isfile('save.dat') is not True:
+        if os.path.isfile(self.save_data_name) is not True:
             return None
 
-        with open('save.dat', 'rb') as file:
+        save_data = None
+
+        with open(self.save_data_name, 'rb') as file:
             save_data = pickle.load(file)
 
         return save_data
 
     def is_save_empty(self):
-        if os.path.isfile('save.dat') is not True:
-            return None
+        """Checks that does savegame exist
+
+        Returns:
+
+            bool: Returns True, if file exists. False otherwise.
+
+        """
+        if os.path.isfile(self.save_data_name) is not True:
+            return False
 
         save_data = None
-        with open('save.dat', 'rb') as file:
+        with open(self.save_data_name, 'rb') as file:
             save_data = pickle.load(file)
         return save_data is not None
