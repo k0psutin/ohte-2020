655aca4c3543832db3125333bf1c22dac7c13907
k0psutin <mr.gravez@gmail.com>
2020-12-08 12:25:11 +0200

    Update testing for highscores

--- src/tests/entities/player_test.py
+++ src/tests/entities/player_test.py
@@ -1,11 +1,26 @@
 import unittest
+import os
 
 from entities.player import Player
+from repositories.player_repository import PlayerRepository
 
 
 class TestPlayer(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.player_repository = PlayerRepository(
+            'test.dat', 'testhighscore.dat')
+
     def setUp(self):
         self.player = Player()
+        self.player.player_repository = self.player_repository
+
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.isfile('test.dat'):
+            os.remove('test.dat')
+        if os.path.isfile('testhighscore.dat'):
+            os.remove('testhighscore.dat')
 
     def test_calling_constructor_creates_an_object(self):
         self.assertNotEqual(self.player, None)
@@ -37,19 +52,21 @@ class TestPlayer(unittest.TestCase):
         self.player.remove_credits(11)
         self.assertEqual(self.player.credits, 0)
 
-    def test_successful_double_increases_streak(self):
+    def test_successful_double_increases_beast_streak(self):
         self.player.successful_double(True)
-        self.assertEqual(self.player.current_double_streak, 1)
+        self.player.double_win(0)
+        self.assertEqual(self.player.best_double_streak, 1)
 
     def test_unsuccessful_double_resets_streak(self):
+        self.player.successful_double(True)
         self.player.successful_double(False)
-        self.assertEqual(self.player.current_double_streak, 0)
+        self.assertEqual(self.player.best_double_streak, 0)
 
     def test_unsuccessful_double_saves_best_streak(self):
         for _ in range(0, 5):
             self.player.successful_double(True)
 
-        self.player.successful_double(False)
+        self.player.double_win(0)
 
         self.assertEqual(self.player.best_double_streak, 5)
 
@@ -57,11 +74,27 @@ class TestPlayer(unittest.TestCase):
         for _ in range(0, 5):
             self.player.successful_double(True)
 
-        self.player.successful_double(False)
+        self.player.double_win(0)
 
         for _ in range(0, 3):
             self.player.successful_double(True)
 
-        self.player.successful_double(False)
+        self.player.double_win(0)
 
         self.assertEqual(self.player.best_double_streak, 5)
+
+    def test_if_double_win_is_the_new_best_it_is_saved(self):
+        self.player.best_double_win = 0
+        self.player.double_win(10)
+        self.assertEqual(self.player.best_double_win, 10)
+
+    def test_player_save_returns_correct_data_when_loaded(self):
+        self.player.save_player()
+        test_load = self.player.load_player()
+        self.assertEqual(self.player, test_load)
+
+    def test_player_save_returns_none_if_player_saved_had_0_credits(self):
+        self.player.credits = 0
+        self.player.save_player()
+        test_load = self.player.load_player()
+        self.assertEqual(test_load, None)
--- src/tests/repositories/player_repository_test.py
+++ src/tests/repositories/player_repository_test.py
@@ -1,39 +1,45 @@
 import unittest
 import os
 from repositories.player_repository import PlayerRepository
+from entities.player import Player
 
 
 class TestPlayerRepository(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls._player_repository = PlayerRepository('test.dat')
+        cls._player_repository = PlayerRepository(
+            'test.dat', 'testhighscore.dat')
 
     def setUp(self):
         if os.path.isfile('test.dat'):
             os.remove('test.dat')
+        if os.path.isfile('testhighscore.dat'):
+            os.remove('testhighscore.dat')
+        self.player = Player()
+        self.player.player_repository = self._player_repository
 
     @classmethod
     def tearDownClass(cls):
         if os.path.isfile('test.dat'):
             os.remove('test.dat')
-
-    def test_is_save_empty_returns_false_if_file_not_present(self):
-        self.assertFalse(self._player_repository.is_save_empty())
+        if os.path.isfile('testhighscore.dat'):
+            os.remove('testhighscore.dat')
 
     def test_is_save_empty_returns_true_if_file_present(self):
         self._player_repository.save('Test')
         self.assertTrue(self._player_repository.is_save_empty())
 
-    def test_load_returns_none_if_no_file_exists(self):
-        test_load = self._player_repository.load()
-        self.assertIs(test_load, None)
+    def test_if_player_has_a_new_highscore_return_true(self):
+        self.player.best_credits = 100
+        self.assertTrue(self._player_repository.is_new_highscore(
+            self.player.best_credits))
 
-    def test_load_returns_none_if_file_exists_but_is_empty(self):
-        self._player_repository.save(None)
-        test_load = self._player_repository.load()
-        self.assertIs(test_load, None)
+    def test_if_player_doesnt_have_a_new_highscore_return_false(self):
+        self.player.best_credits = 10
+        self.assertFalse(self._player_repository.is_new_highscore(
+            self.player.best_credits))
 
-    def test_load_returns_true_if_file_exists_and_is_not_empty(self):
-        self._player_repository.save(True)
-        test_load = self._player_repository.load()
-        self.assertIs(test_load, True)
+    def test_submit_highscore_returns_correct_highscore(self):
+        self.player.best_credits = 210
+        self.player.submit_highscore('AAA')
+        self.assertEqual(self._player_repository.highscores[9]['name'], 'AAA')
